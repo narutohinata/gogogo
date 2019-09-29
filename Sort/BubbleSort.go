87c7b5864d284505eec954742a6585e4d9e0688e
chromer <chuliang2013@live.cn>
2019-09-29 21:55:34 +0800

    feature Add BubbleSort

---
+++ Sort/BubbleSort.go
@@ -0,0 +1,13 @@
+package Sort
+
+func BubbleSort(data []int)  {
+	for i := len(data) - 1; i > 0; i-- {
+		for j := 0; j < i; j++{
+			if data[j] > data[j+1] {
+				temp := data[j]
+				data[j] = data[j+1]
+				data[j+1] = temp
+			}
+		}
+	}
+}

8448e75903f966b98b74bb6f8c1760a63675a3dc
chromer <chuliang2013@live.cn>
2019-10-01 23:13:50 +0800

    optimize BubboleSort

--- Sort/BubbleSort.go
+++ Sort/BubbleSort.go
@@ -13,3 +13,36 @@ func BubbleSort(data []int)  {
 		}
 	}
 }
+
+func BubbleSortVersion2(data []int)  {
+	for i := len(data) - 1; i > 0; i-- { // n
+		sorted := true // global sorted
+		for j := 0; j < i; j++{ // n
+			if data[j] > data[j+1] {
+				temp := data[j]
+				data[j] = data[j+1]
+				data[j+1] = temp
+				sorted = false
+			}
+		}
+		// if global sorted is true stop bubble
+		if sorted {
+			return
+		}
+	}
+}
+
+func BubbleSortVersion3(data []int)  {
+	for i := len(data) - 1; i > 0; i-- { // n
+		sortedIndex := i
+		for j := 0; j < i; j++{ // n
+			if data[j] > data[j+1] {
+				temp := data[j]
+				data[j] = data[j+1]
+				data[j+1] = temp
+				sortedIndex = j+1
+			}
+		}
+		i = sortedIndex
+	}
+}

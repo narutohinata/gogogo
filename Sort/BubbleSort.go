1be7e3a26d8e2cb532e0c19c223451ca316acbb9
chromer <chuliang2013@live.cn>
2019-10-01 20:13:04 +0800

    add comment for bubblesort.go

--- Sort/BubbleSort.go
+++ Sort/BubbleSort.go
@@ -1,8 +1,10 @@
 package Sort
 
+// Base Version
+// O(n^2)
 func BubbleSort(data []int)  {
-	for i := len(data) - 1; i > 0; i-- {
-		for j := 0; j < i; j++{
+	for i := len(data) - 1; i > 0; i-- { // n
+		for j := 0; j < i; j++{ // n
 			if data[j] > data[j+1] {
 				temp := data[j]
 				data[j] = data[j+1]

12d3ca16d82200f3d98d9e3256b4976dc2c848ec
chromer <chuliang2013@live.cn>
2019-10-04 17:04:40 +0800

    feature add selectSort

--- Sort/SelectSort.go
+++ Sort/SelectSort.go
@@ -1,1 +1,17 @@
 package Sort
+
+//worst O(n^2)
+//best O(n^2)
+func SelectSort(data []int)  {
+	for i := 0; i < len(data) -1;  i++{
+		min := data[i]
+		for j := i+1 ; j < len(data); j++ {
+			if data[j] < min {
+				temp := data[j]
+				data[j] = min
+				min = temp
+			}
+		}
+		data[i] = min
+	}
+}

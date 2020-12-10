51ee48e4037ee359cddaeae934a7eadec4d29a2a
chromer <chuliang2013@live.cn>
2020-12-10 10:45:42 +0800

    Update InsertSort.go

--- Sort/InsertSort.go
+++ Sort/InsertSort.go
@@ -3,8 +3,8 @@ package Sort
 func InsertSort(data []int)  {
 	for i := 1; i < len(data); i++ {
 		key := data[i]
-		j := data[i-1]
-		for j >= 0 && data[j] > key{
+		
+		for j := i-1; j >= 0 && data[j] > key{
 			data[j+1] = data[j]
 			j--
 		}
